5b401bd262df1fe051ff8b3577b200295a67d760
JC <cabilijc@gmail.com>
2018-10-03 21:56:21 +0800

    Initial src upload

---
+++ GoogleMaps/GoogleMaps/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace GoogleMaps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // map
+            // 
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemmory = 5;
+            this.map.Location = new System.Drawing.Point(0, 0);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 2;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(961, 720);
+            this.map.TabIndex = 0;
+            this.map.Zoom = 0D;
+            this.map.MouseDown += new System.Windows.Forms.MouseEventHandler(this.map_MouseDown);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "14.676041";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "121.043700";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Routes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 161);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Routes";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(818, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(143, 720);
+            this.panel1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(961, 720);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.map);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl map;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ GoogleMaps/GoogleMaps/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.WindowsForms;
+using System.Runtime.Serialization;
+
+namespace GoogleMaps
+{
+    public partial class Form1 : Form
+    {
+        GMapOverlay markers = new GMapOverlay("markers");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            map.MapProvider = GMapProviders.GoogleMap;
+            double lat = Convert.ToDouble(textBox1.Text);
+            double longt = Convert.ToDouble(textBox2.Text);
+            map.Position = new PointLatLng(lat,longt);
+            map.MinZoom = 2;
+            map.MaxZoom = 15;
+            map.Zoom = 15;
+
+        }
+
+      
+
+        private void map_MouseDown(object sender, MouseEventArgs e)
+        {
+            double X = map.FromLocalToLatLng(e.X, e.Y).Lng;
+            double Y = map.FromLocalToLatLng(e.X, e.Y).Lat;
+
+
+            var bmp = new Bitmap(GoogleMaps.Properties.Resources.parking);
+
+
+            GMapMarker marker = new GMarkerGoogle(new PointLatLng(Y, X), GMarkerGoogleType.red_dot);
+
+           // var size = new Size(25,25);
+
+           // marker.Size = size;
+            
+            marker.ToolTipText = "Emergency Request! \nTeller:Juan Delacruz \nTerminal:Entrance1";
+            marker.ToolTipMode = MarkerTooltipMode.Always;
+
+
+            marker.ToolTip.Fill = Brushes.GhostWhite;
+            marker.ToolTip.Foreground = Brushes.Black;
+            marker.ToolTip.Stroke = Pens.White;
+            marker.ToolTip.TextPadding = new Size(10, 10);
+            
+
+
+            Font font = new Font("Tahoma",14);
+            marker.ToolTip.Font = font;
+            
+
+            markers.Markers.Add(marker);
+            map.Overlays.Add(markers);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PointLatLng start = new PointLatLng(14.676041, 121.043700);
+            PointLatLng end = new PointLatLng(14.681922, 121.042110);
+            MapRoute route = GMap.NET.MapProviders.GoogleMapProvider.Instance.GetRoute(
+              start, end, false, false, 15);
+
+            GMapRoute r = new GMapRoute(route.Points, "My route");
+
+            GMapOverlay routesOverlay = new GMapOverlay("routes");
+            routesOverlay.Routes.Add(r);
+            map.Overlays.Add(routesOverlay);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            markers.Markers.Clear();
+            map.Overlays.Clear();
+        }
+    }
+
+    public class GmapMarkerWithLabel : GMapMarker, ISerializable
+    {
+        private Font font;
+        private GMarkerGoogle innerMarker;
+
+        public string Caption;
+
+        public GmapMarkerWithLabel(PointLatLng p, string caption, GMarkerGoogleType type)
+            : base(p)
+        {
+            font = new Font("Arial", 14);
+            innerMarker = new GMarkerGoogle(p, type);
+
+            Caption = caption;
+        }
+
+        public override void OnRender(Graphics g)
+        {
+            if (innerMarker != null)
+            {
+                innerMarker.OnRender(g);
+            }
+
+            g.DrawString(Caption, font, Brushes.Black,new PointF(0.0f, innerMarker.Size.Height));
+        }
+
+        public override void Dispose()
+        {
+            if (innerMarker != null)
+            {
+                innerMarker.Dispose();
+                innerMarker = null;
+            }
+
+            base.Dispose();
+        }
+
+        #region ISerializable Members
+
+        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+        }
+
+        protected GmapMarkerWithLabel(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        #endregion
+    }
+}
